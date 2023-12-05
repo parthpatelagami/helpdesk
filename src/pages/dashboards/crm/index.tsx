6c9bb4ec17f015083184a0a91f9a81a171342db5
nimitdesai2000 <nimit.desai@agami-tech.com>
2023-12-05 16:22:57 +0530

    Skeleton Added For Ticket Count Card

--- src/pages/dashboards/crm/index.tsx
+++ src/pages/dashboards/crm/index.tsx
@@ -1,5 +1,10 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
 // ** MUI Import
 import Grid from '@mui/material/Grid'
+import { Skeleton, Box } from '@mui/material'
+import Card from '@mui/material/Card'
 
 // ** Demo Component Imports
 import CrmSessions from 'src/views/dashboards/crm/CrmSessions'
@@ -19,11 +24,47 @@ import CardStatsVertical from 'src/@core/components/card-statistics/card-stats-v
 import CardStatistics from 'src/views/dashboards/crm/CrmTicketsCount'
 
 const CrmDashboard = () => {
+  const [res, setRes] = useState<boolean>(false)
+
+  useEffect(() => {
+    axios
+      .get('https://dummyjson.com/products')
+      .then(response => {
+        setRes(true)
+      })
+      .catch(error => {
+        console.log(error)
+        setRes(false)
+      })
+  }, [])
+
   return (
     <ApexChartWrapper>
       <Grid container spacing={6}>
         <Grid item xs={12}>
-          <CardStatistics />
+          {res ? (
+            <CardStatistics />
+          ) : (
+            <Card sx={{ height: 150 }}>
+              <Box sx={{ marginTop: 6, marginLeft: 5 }}>
+                <Skeleton width={190} sx={{ marginLeft: 2 }} />
+              </Box>
+              <Box sx={{ display: 'flex', marginTop: 10, marginLeft: 5 }}>
+                <Skeleton animation='wave' variant='circular' width={40} height={40} sx={{ marginLeft: 2 }} />
+                <Skeleton width={190} sx={{ marginLeft: 2 }} />
+                <Skeleton animation='wave' variant='circular' width={40} height={40} sx={{ marginLeft: 2 }} />
+                <Skeleton width={190} sx={{ marginLeft: 2 }} />
+                <Skeleton animation='wave' variant='circular' width={40} height={40} sx={{ marginLeft: 2 }} />
+                <Skeleton width={190} sx={{ marginLeft: 2 }} />
+                <Skeleton animation='wave' variant='circular' width={40} height={40} sx={{ marginLeft: 2 }} />
+                <Skeleton width={190} sx={{ marginLeft: 2 }} />
+                <Skeleton animation='wave' variant='circular' width={40} height={40} sx={{ marginLeft: 2 }} />
+                <Skeleton width={190} sx={{ marginLeft: 2 }} />
+                <Skeleton animation='wave' variant='circular' width={40} height={40} sx={{ marginLeft: 2 }} />
+                <Skeleton width={190} sx={{ marginLeft: 2 }} />
+              </Box>
+            </Card>
+          )}
         </Grid>
       </Grid>
     </ApexChartWrapper>
